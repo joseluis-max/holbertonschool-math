89d2330e68ce60461d48a9362bf05071dc5ca3b7
Jose Luis <luisj.valdiviezo20@gmail.com>
2021-07-30 22:53:03 -0500

    c != NULL

--- 0x00-math_complex/0-display.c
+++ 0x00-math_complex/0-display.c
@@ -1,15 +1,19 @@
 #include "holberton.h"
 #include <stdio.h>
+#include <stddef.h>
 /**
  * display_complex_number - printf a complex number
  * c: struct type complex
  */
 void display_complex_number(complex c)
 {
-	printf("%0.f", c.re);
-	if (c.im != 0)
+	if (c != NULL)
 	{
-		printf(" %c %0.fi", SIGN, c.im);
+		printf("%0.f", c.re);
+		if (c.im != 0)
+		{
+			printf(" %c %0.fi", SIGN, c.im);
+		}
+		printf("\n");
 	}
-	printf("\n");
 }
