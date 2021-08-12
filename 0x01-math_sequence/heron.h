3fd5fe2c48118d32673434ffc83c469891c3dc98
Jose Luis <luisj.valdiviezo20@gmail.com>
2021-08-12 01:23:14 -0500

    betty styles in .h

--- 0x01-math_sequence/heron.h
+++ 0x01-math_sequence/heron.h
@@ -3,10 +3,10 @@
 /**
  * struct cell - describe number and approximation of heron'r sequence
  * @p:root number of found
- * @xn: current iteration aproximation
+ * @elt: current iteration aproximation
  * @next: next element in linked list
  */
-typedef struct cell 
+typedef struct cell
 {
 	double p;
 	double elt;
